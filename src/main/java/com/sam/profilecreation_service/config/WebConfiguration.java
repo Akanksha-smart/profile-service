9341e40bfb44ebc355f6b9927348948768f1c962
subham sharma <subham.sharma@smartbear.com>
2024-09-04 17:14:49 +0530

    cors

--- src/main/java/com/sam/profilecreation_service/config/WebConfiguration.java
+++ src/main/java/com/sam/profilecreation_service/config/WebConfiguration.java
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
 
         @Override
-        public void addCorsMappings(CorsRegistry registry)
-        {
+        public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
-                    .allowedHeaders("http://localhost:3000")
+                    .allowedHeaders("*")
                     .allowCredentials(true);
         }
 
